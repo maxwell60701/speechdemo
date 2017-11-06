ba9e2a3b2ce5b6260b9664a49a5f6e575b31a2f8
maxwell <1007499575@qq.com>
2017-11-06 10:58:15 +0800

    ewewe

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using Baidu.Aip.Demo;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = ConfigurationManager.AppSettings["path"].ToString();
+            SpeechDemo speechdemo = new SpeechDemo();
+           // speechdemo.AsrData(path);
+            speechdemo.Tts();
+        }
+    }
+}
---
+++ ConsoleApp2/SpeechDemo.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Baidu.Aip.Speech;
+using System.Configuration;
+
+namespace Baidu.Aip.Demo
+{
+    public class SpeechDemo
+    {
+        private readonly Asr _asrClient;
+        private readonly Tts _ttsClient;
+        private string apikey = ConfigurationManager.AppSettings["ApiKey"].ToString();
+        private string secretkey = ConfigurationManager.AppSettings["SecretKey"].ToString();
+        private string recordpath = ConfigurationManager.AppSettings["recordpath"].ToString();
+        private string message = ConfigurationManager.AppSettings["message"].ToString();
+        public SpeechDemo()
+        {
+            _asrClient = new Asr(apikey, secretkey);
+            _ttsClient = new Tts(apikey, secretkey);
+        }
+
+        // 识别本地文件
+        public void AsrData(string path)
+        {
+            var data = File.ReadAllBytes(path);
+            var result = _asrClient.Recognize(data, "pcm", 16000);
+            Console.Write(result);
+            Console.ReadKey();
+        }
+
+        // 识别URL中的语音文件
+        public void AsrUrl()
+        {
+            var result = _asrClient.Recoginze(
+                "http://xxx.com/待识别的pcm文件地址",
+                "http://xxx.com/识别结果回调地址",
+                "pcm",
+                16000);
+            Console.WriteLine(result);
+        }
+
+        // 合成
+        public void Tts()
+        {
+            // 可选参数
+            var option = new Dictionary<string, object>()
+            {
+                {"spd", 5}, // 语速
+                {"vol", 7}, // 音量
+                {"per", 4}  // 发音人，4：情感度丫丫童声
+            };
+            var result = _ttsClient.Synthesis(message, option);
+
+            if (result.ErrorCode == 0)  // 或 result.Success
+            {
+                File.WriteAllBytes(recordpath, result.Data);
+            }
+        }
+
+    }
+}
